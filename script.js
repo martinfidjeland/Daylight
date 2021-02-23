912624b6eb606a0f70e96498116e326365bfe5d4
Martin <martinfidjeland@gmail.com>
2021-02-23 13:21:55 +0100

    Updated site.

--- script.js
+++ script.js
@@ -1,74 +1,95 @@
-function load() {
-    let long;
-    let lat;
-}
-
 console.log(`This application is inspired by Bakken & Bæcks' Daylight.`);
 
-const headerTitle = document.getElementById('header-title');
-const headerSubtitle = document.getElementById('header-subtitle');
+function load() {
+  let long;
+  let lat;
+}
+
+const title = document.getElementById("title");
+const subtitle = document.getElementById("sub-title");
+const rises = document.getElementById("rises");
+const sets = document.getElementById("sets");
 
 if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
+  navigator.geolocation.getCurrentPosition((position) => {
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
 
-        const date = new Date();
-        const dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().split("T")[0];
-        const tomorrowDateString = new Date(date.setDate(date.getDate() + 1) - (date.getTimezoneOffset() * 60000)).toISOString().split("T")[0];
+    const date = new Date();
+    const dateString = new Date(
+      date.getTime() - date.getTimezoneOffset() * 60000
+    )
+      .toISOString()
+      .split("T")[0];
 
-        let offset = (date.getTimezoneOffset() / -60);
-        offset = `0${offset}:00`;
+    const yesterdayDateString = new Date(
+      date.setDate(date.getDate() + 1) - date.getTimezoneOffset() * 60000
+    )
+      .toISOString()
+      .split("T")[0];
 
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
+    let offset = date.getTimezoneOffset() / -60;
+    offset = `0${offset}:00`;
 
-        Promise.all([
-            fetch(`https://api.met.no/weatherapi/sunrise/2.0/.json?lat=${lat}&lon=${long}&date=${dateString}&offset=${offset}`).then(response => response.json()),
-            fetch(`https://api.met.no/weatherapi/sunrise/2.0/.json?lat=${lat}&lon=${long}&date=${tomorrowDateString}&offset=${offset}`).then(response => response.json())
+    const proxy = "https://cors-anywhere.herokuapp.com/";
 
-        ]).then(data => {
-            const sunriseToday = data[0].location.time[0].sunrise.time.slice(11, -9).replace(/:/, '');
-            const sunsetToday = data[0].location.time[0].sunset.time.slice(11, -9).replace(/:/, '');
-            const sunriseTomorrow = data[1].location.time[0].sunrise.time.slice(11, -9).replace(/:/, '');
-            const sunsetTomorrow = data[1].location.time[0].sunset.time.slice(11, -9).replace(/:/, '');
+    Promise.all([
+      fetch(
+        `https://api.met.no/weatherapi/sunrise/2.0/.json?lat=${lat}&lon=${long}&date=${dateString}&offset=${offset}`
+      ).then((response) => response.json()),
+      fetch(
+        `https://api.met.no/weatherapi/sunrise/2.0/.json?lat=${lat}&lon=${long}&date=${yesterdayDateString}&offset=${offset}`
+      ).then((response) => response.json()),
+    ]).then((data) => {
+      const sunriseToday = data[0].location.time[0].sunrise.time
+        .slice(11, -9)
+        .replace(/:/, "");
+      const sunsetToday = data[0].location.time[0].sunset.time
+        .slice(11, -9)
+        .replace(/:/, "");
+      const sunriseYesterday = data[1].location.time[0].sunrise.time
+        .slice(11, -9)
+        .replace(/:/, "");
+      const sunsetYesterday = data[1].location.time[0].sunset.time
+        .slice(11, -9)
+        .replace(/:/, "");
 
-            let today = +sunsetToday - +sunriseToday;
-            let tomorrow = +sunsetTomorrow - +sunriseTomorrow;
-            let diff = tomorrow - today;
+      let diff =
+        +sunsetYesterday - +sunriseYesterday - (+sunsetToday - +sunriseToday);
 
-            let y;
+      if (diff < 0) {
+        diff = Math.abs(diff);
+      }
 
-            if (diff < 0) {
-                y = Math.abs(diff);
-            }
+      let min;
 
-            let min;
-            let isAre;
+      if (diff === 1) {
+        min = `${diff} minute`;
+      } else {
+        min = `${diff} minutes`;
+      }
 
-            if (y === 1) {
-                min = `${y} minute`;
-                isAre = 'is'
-            } else {
-                min = `${y} minutes`;
-                isAre = 'are'
-            }
+      if (+sunsetYesterday - +sunriseYesterday < +sunsetToday - +sunriseToday) {
+        title.innerHTML = `Today we lost ${min} of sunlight.`;
+      } else {
+        title.innerHTML = `Today we gained ${min} of additional sunshine.`;
+      }
 
-            if (today > tomorrow) {
-                headerTitle.innerHTML = `Tomorrow there ${isAre} ${min} less sunlight than today.`;
-            } else {
-                headerTitle.innerHTML = `Tomorrow there ${isAre} ${min} more sunlight than today.`;
-            }
-
-            document.getElementById('header-subtitle').innerHTML = `The sun rises at ${data[0].location.time[0].sunrise.time.slice(11, -9)} today and sets at ${data[0].location.time[0].sunset.time.slice(11, -9)}.`;
-        });
+      rises.innerHTML = data[0].location.time[0].sunrise.time.slice(11, -9);
+      sets.innerHTML = data[0].location.time[0].sunset.time.slice(11, -9);
     });
-} 
+  });
+}
 
-navigator.permissions && navigator.permissions.query({
-    name: 'geolocation'
-}) .then(function (PermissionStatus) {
-    if (PermissionStatus.state == 'granted') {
-    } else if (PermissionStatus.state == 'prompt') {
-        headerTitle.innerHTML = 'Location must be enabled for this application to work.';
-    }
-})
+navigator.permissions &&
+  navigator.permissions
+    .query({
+      name: "geolocation",
+    })
+    .then(function (PermissionStatus) {
+      if (PermissionStatus.state == "granted") {
+      } else if (PermissionStatus.state == "prompt") {
+        title.innerHTML =
+          "Location must be enabled for this application to work.";
+      }
+    });
